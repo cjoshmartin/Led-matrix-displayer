6eb1fea67863e8affe07b698010fa38cb99461d0
Josh Martin <contact@cjoshmartin.com>
2019-05-05 11:34:42 -0400

    Text is appearing on image

--- image-viewer.py
+++ image-viewer.py
@@ -3,20 +3,24 @@ import time
 import sys
 
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
-from PIL import Image,ImageOps
+from PIL import Image, ImageFont, ImageDraw, ImageOps
 import requests
 from io import BytesIO
 
 mask = Image.open('mask.png').convert('L')
 
 url = "https://venmopics.appspot.com/u/v1/m/732a8c89-05be-46e3-bdbe-6153a30e95b4"
-name = "Josh Martin"
+name = "@Josh"
 response = requests.get(url)
 image = Image.open(BytesIO(response.content))
+
 
 image  = ImageOps.fit(image, mask.size, centering=(0.5, 0.5))
 image.putalpha(mask)
 
+draw = ImageDraw.Draw(image)
+font = ImageFont.truetype("FreeSans.ttf", 30)
+draw.text((10, 50), name,(255,0,0),font=font)
 # Configuration for the matrix
 options = RGBMatrixOptions()
 options.rows = 32
@@ -28,7 +32,7 @@ options.brightness= 60
 matrix = RGBMatrix(options = options)
 
 # Make image fit our screen.
-image.thumbnail((matrix.width/2, matrix.height/2), Image.ANTIALIAS)
+image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
 
 matrix.SetImage(image.convert('RGB'))
 
